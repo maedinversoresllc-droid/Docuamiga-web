065710d49c14cce7d707f301f20d9f8d3b35ccc5
maedinversoresllc-droid <maedinversoresllc@gmail.com>
2026-06-03 16:22:55 -0400

    Delete app/blog/page.tsx

--- app/blog/page.tsx
+++
@@ -1,8 +0,0 @@
-import ClientPage from "./ClientPage";
-
-// ¡Aquí es donde ponemos la instrucción para que Cloudflare funcione!
-export const runtime = "edge";
-
-export default function Page() {
-  return <ClientPage />;
-}
